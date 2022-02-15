2cd86ffff3fbb68728407d49a9c337f4101ccfb1
Geoffrey (Dingquan) Yu <geoffrey.yu96@icloud.com>
2022-02-15 17:04:15 +0100

    updated test.py

--- test/test.py
+++ test/test.py
@@ -18,7 +18,7 @@ class TestSum(unittest.TestCase):
 
         test_input = MultimericInput(test_feature_dict,test_protein_names)
         test_input.create_monomeric_objects()
-        output_aatype = test_input.concatenate_template_aatype()
+        output_dict = test_input.create_concatenated_template_features()
 
         total_num_templates = 0
         for m in test_input.monomers:
@@ -31,8 +31,49 @@ class TestSum(unittest.TestCase):
         print(f"Total number of templates is {total_num_templates}")
         print(f"Final sequence length is {total_seq_length}")
 
-        self.assertEqual((total_num_templates,total_seq_length,22),output_aatype.shape)
+        self.assertEqual((total_num_templates,total_seq_length,22),output_dict['template_aatype'].shape)
+
+    def test_concatenate_atom_mask(self):
+        """test aatype concatenation"""
+
+        test_input = MultimericInput(test_feature_dict,test_protein_names)
+        test_input.create_monomeric_objects()
+        output_dict = test_input.create_concatenated_template_features()
+
+        total_num_templates = 0
+        for m in test_input.monomers:
+            total_num_templates += m.template_feature_dict['template_all_atom_masks'].shape[0]
+        
+        total_seq_length = 0
+        for m in test_input.monomers:
+            total_seq_length += m.template_feature_dict['template_all_atom_masks'].shape[1]
+
+        print(f"Total number of templates is {total_num_templates}")
+        print(f"Final sequence length is {total_seq_length}")
 
 
+        self.assertEqual((total_num_templates,total_seq_length,37),output_dict['template_all_atom_masks'].shape)
+
+    def test_concatenate_atom_pos(self):
+        """test aatype concatenation"""
+
+        test_input = MultimericInput(test_feature_dict,test_protein_names)
+        test_input.create_monomeric_objects()
+        output_dict = test_input.create_concatenated_template_features()
+
+        total_num_templates = 0
+        for m in test_input.monomers:
+            total_num_templates += m.template_feature_dict['template_all_atom_masks'].shape[0]
+        
+        total_seq_length = 0
+        for m in test_input.monomers:
+            total_seq_length += m.template_feature_dict['template_all_atom_masks'].shape[1]
+
+        print(f"Total number of templates is {total_num_templates}")
+        print(f"Final sequence length is {total_seq_length}")
+
+
+        self.assertEqual((total_num_templates,total_seq_length,37,3),output_dict['template_all_atom_positions'].shape)
+    
 if __name__ == '__main__':
     unittest.main()
